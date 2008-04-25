f5c8779737d479bcd616b8f790de6e4f40cc2298
doglioni <>
2008-04-25 10:43:17 +0000

    Polished code/indentation

--- plugins/EcalChannelAnalyzer.cc
+++ plugins/EcalChannelAnalyzer.cc
@@ -5,14 +5,14 @@
 // 
 /**\class EcalChannelAnalyzer EcalChannelAnalyzer.cc
 
- Description: <one line class summary>
+Description: <one line class summary>
 
- Implementation:
-     <Notes on implementation>
-*/
+Implementation:
+<Notes on implementation>
+ */
 // 
 // Original Author:  Caterina DOGLIONI
-//         Created:  Th Nov 22 5:46:22 CEST 2007
+//         Created:  Tu Apr 22 5:46:22 CEST 2008
 // $Id$
 //
 //
@@ -25,16 +25,9 @@
 #include "TFile.h"
 #include "TH1F.h"
 
+#define MAX_XTALS 61200
+
 using namespace edm;
-using namespace std;
-
-//
-// constants, enums and typedefs
-//
-
-//
-// static data member definitions
-//
 
 //
 // constructors and destructor
@@ -43,64 +36,64 @@ EcalChannelAnalyzer::EcalChannelAnalyzer(const edm::ParameterSet& iConfig)
 	:inputTreeFileName_ (iConfig.getUntrackedParameter<std::string>("inputTreeFileName","")),
 	inputHistoFileName_(iConfig.getUntrackedParameter<std::string>("inputHistoFileName","")),
 	outputFileName_(iConfig.getUntrackedParameter<std::string>("outputFileName","")),
-	v_cuts_ (iConfig.getUntrackedParameter< vector<std::string> >  ("v_cuts") )
+	v_cuts_ (iConfig.getUntrackedParameter< std::vector<std::string> >  ("v_cuts") )
 
 {
-//--- INITIALIZATIONS 
-nCuts_=v_cuts_.size();
-initHistTypeMaps();
+	//--- INITIALIZATIONS 
+	nCuts_=v_cuts_.size();
+	initHistTypeMaps();
 
-//--- CLEANING OF VECTORS
-for (int i=0; i < 61200; i++) {	
-	xtalBitmask_[i] = 0;
+	//--- CLEANING OF VECTORS
+	for (int i=0; i < MAX_XTALS; i++) {	
+		xtalBitmask_[i] = 0;
+	}
+
+	//--- OPENING FILES
+
+	//TODO: try/catch 
+	fin_tree_= TFile::Open(inputTreeFileName_.c_str(),"READ");
+	if (fin_tree_->IsZombie()) {
+		std::cout << "Error opening tree file" << std::endl;
+		exit(-1);
+	}
+
+	//TODO: try/catch 
+	fin_histo_= TFile::Open(inputHistoFileName_.c_str(),"READ");
+	if (fin_histo_->IsZombie()) {
+		std::cout << "Error opening histo file" << std::endl;
+		exit(-1);
+	}
+
+	//TODO: try/catch
+	fout_=TFile::Open(outputFileName_.c_str(),"RECREATE");
+	if (fout_->IsZombie()) {
+		std::cout << "Error opening output file" << std::endl;
+		exit(-1);
+	}
+
+	//--- INIT TREE
+
+	//TODO:name of tree from .cfg + try/catch 
+	t_=(TTree*)fin_tree_->Get("xtal_tree");
+
+	//std::cout << "treename=" << t_->GetName() << std::endl;
+
+	t_->SetBranchAddress("ic", &ic, &b_ic);
+	t_->SetBranchAddress("ism", &ism, &b_ism);
+	t_->SetBranchAddress("ieta", &ieta, &b_ieta);
+	t_->SetBranchAddress("iphi", &iphi, &b_iphi);
+	t_->SetBranchAddress("hashedIndex", &hashedIndex, &b_hashedIndex);
+	t_->SetBranchAddress("ped_avg", &ped_avg, &b_ped_avg);
+	t_->SetBranchAddress("ped_rms", &ped_rms, &b_ped_rms);
+	t_->SetBranchAddress("ampli_avg", &ampli_avg, &b_ampli_avg);
+	t_->SetBranchAddress("ampli_rms", &ampli_rms, &b_ampli_rms);
+	t_->SetBranchAddress("jitter_avg", &jitter_avg, &b_jitter_avg);
+	t_->SetBranchAddress("jitter_rms", &jitter_rms, &b_jitter_rms);
+	t_->SetBranchAddress("ampli_fracBelowThreshold", &ampli_fracBelowThreshold, &b_ampli_fracBelowThreshold);
+	t_->SetBranchAddress("entries", &entries, &b_entries);
+
 }
- 
-//--- OPENING FILES
 
-//TODO: try/catch 
-fin_tree_= TFile::Open(inputTreeFileName_.c_str(),"READ");
-if (fin_tree_->IsZombie()) {
-       cout << "Error opening tree file" << endl;
-       exit(-1);
-}
-
-//TODO: try/catch 
-fin_histo_= TFile::Open(inputHistoFileName_.c_str(),"READ");
-if (fin_histo_->IsZombie()) {
-       cout << "Error opening histo file" << endl;
-       exit(-1);
-}
-
-//TODO: try/catch
-fout_=TFile::Open(outputFileName_.c_str(),"RECREATE");
-if (fout_->IsZombie()) {
-       cout << "Error opening output file" << endl;
-       exit(-1);
-}
-
-//--- INIT TREE
-
-//TODO:name of tree from .cfg + try/catch 
-t_=(TTree*)fin_tree_->Get("xtal_tree");
-
-//std::cout << "treename=" << t_->GetName() << std::endl;
-
-t_->SetBranchAddress("ic", &ic, &b_ic);
-t_->SetBranchAddress("ism", &ism, &b_ism);
-t_->SetBranchAddress("ieta", &ieta, &b_ieta);
-t_->SetBranchAddress("iphi", &iphi, &b_iphi);
-t_->SetBranchAddress("hashedIndex", &hashedIndex, &b_hashedIndex);
-t_->SetBranchAddress("ped_avg", &ped_avg, &b_ped_avg);
-t_->SetBranchAddress("ped_rms", &ped_rms, &b_ped_rms);
-t_->SetBranchAddress("ampli_avg", &ampli_avg, &b_ampli_avg);
-t_->SetBranchAddress("ampli_rms", &ampli_rms, &b_ampli_rms);
-t_->SetBranchAddress("jitter_avg", &jitter_avg, &b_jitter_avg);
-t_->SetBranchAddress("jitter_rms", &jitter_rms, &b_jitter_rms);
-t_->SetBranchAddress("ampli_fracBelowThreshold", &ampli_fracBelowThreshold, &b_ampli_fracBelowThreshold);
-t_->SetBranchAddress("entries", &entries, &b_entries);
-
-}
-	
 EcalChannelAnalyzer::~EcalChannelAnalyzer()
 {
 
@@ -110,84 +103,91 @@ EcalChannelAnalyzer::~EcalChannelAnalyzer()
 // member functions
 //
 
-// ------------ method called to for each event  ------------
+// ------------ method called for each event  ------------
 
-void
+	void
 EcalChannelAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-//do nothing!
+	//do nothing!
 }//end analyze
 
 
 // ------------ method called once each job just before starting event loop  ------------
-void 
+	void 
 EcalChannelAnalyzer::beginJob(const edm::EventSetup&)
 {
 
-//filling event list vector with lists produced by cuts
-//by construction, list made from cut in v_cuts_[i] is in v_eventList_[i] 
-fillEventListVector(v_cuts_);
+	//filling event list vector with lists produced by cuts
+	//by construction, list made from cut in v_cuts_[i] is in v_eventList_[i] 
+	fillEventListVector(v_cuts_);
 
-//loop on all event lists 
-for (unsigned int i=0; i<v_eventList_.size(); i++){
-	//loop on events in event lists
-	for (int j=0; j<v_eventList_[i].GetN(); j++) {
+	//loop on all event lists 
+	for (unsigned int i=0; i<v_eventList_.size(); i++){
+		//loop on events in event lists
+		for (unsigned int j=0; j<(unsigned int)v_eventList_[i].GetN(); j++) {
 
-		t_->GetEntry(v_eventList_[i].GetEntry(j));
-			
-		//if bitmask hasn't been allocated for selected crystal, allocate it
-		if (!xtalBitmask_[hashedIndex]) xtalBitmask_[hashedIndex] = new vector<bool> (v_cuts_.size());
-		//flagging the cut relative to the entryList
-		xtalBitmask_[hashedIndex]->operator[](i)=1;
-		
+			t_->GetEntry(v_eventList_[i].GetEntry(j));
+
+			//if bitmask hasn't been allocated for selected crystal, allocate it
+			if (!xtalBitmask_[hashedIndex]) xtalBitmask_[hashedIndex] = new std::vector<bool> (v_cuts_.size());
+			//flagging the cut relative to the entryList
+			xtalBitmask_[hashedIndex]->operator[](i)=1;
+
+		}
+		//adding current event list to total event list (removing duplicate crystals)
+		totalEventList_.Add(&v_eventList_[i]); 
+
 	}
-	//adding current event list to total event list (removing duplicate crystals)
-	totalEventList_.Add(&v_eventList_[i]); 
 
-}
+	//loop on events in event lists
+	for (unsigned int j=0; j<(unsigned int)totalEventList_.GetN(); j++) {
 
-//loop on events in event lists
-for (int j=0; j<totalEventList_.GetN(); j++) {
+		t_->GetEntry(totalEventList_.GetEntry(j));
+		//dirty trick with enum + implicit converter 
+		for (unsigned int i=0; i<EcalChannelAnalyzer::NTYPES; i++) {
+			writeHistFromFile(hashedIndex, ism, ic, (EcalChannelAnalyzer::n_h1Type)i);
+		} 
 
-	t_->GetEntry(totalEventList_.GetEntry(j));
-	//dirty trick with enum + implicit converter 
-	for (int i=0; i<EcalChannelAnalyzer::NTYPES; i++) {
-		writeHistFromFile(hashedIndex, ism, ic, (EcalChannelAnalyzer::n_h1Type)i);
-	} 
-		
-}
+	}
 
 }
 
 
 
 // ------------ method called once each job just after ending the event loop  ------------
-void 
+	void 
 EcalChannelAnalyzer::endJob() 
 {
 
-//printouts
-std::cout << "------ALL CUTS-----" << std::endl;
+	//printouts
 
-for (unsigned int i=0; i<v_eventList_.size(); i++){
-	v_eventList_[i].Print();
-	t_->SetEventList(&v_eventList_[i]);
+	std::cout << "------ORDERED LIST OF CUTS-----" << std::endl;
+	
+	for (unsigned int i=0; i<v_eventList_.size(); ++i){
+		std::cout << i << ". " << v_eventList_[i].GetTitle() << std::endl; 
+	}
+
+	std::cout << "------LISTS FOR SINGLE CUTS-----" << std::endl;
+
+	for (unsigned int i=0; i<v_eventList_.size(); i++){
+		std::cout << v_eventList_[i].GetTitle() << std::endl; 
+		t_->SetEventList(&v_eventList_[i]);
+		t_->Scan();
+	}
+
+	std::cout << "------ALL CRYSTALS-----" << std::endl;
+
+	t_->SetEventList(&totalEventList_);
 	t_->Scan();
-}
 
-std::cout << "------ALL CRYSTALS-----" << std::endl;
-  
-t_->SetEventList(&totalEventList_);
-t_->Scan();
+	//cleanup
+	for (unsigned int i=0; i<MAX_XTALS; i++) {
+		if (xtalBitmask_[i]!=0)  delete xtalBitmask_[i];
+	}
 
-//cleanup
-for (int i=0; i<61200; i++) {
-	if (xtalBitmask_[i]!=0)  delete xtalBitmask_[i];
-}
-
-fin_tree_->Close();
-fin_histo_->Close();
-fout_->Close();
+	fin_tree_->Close();
+	fin_histo_->Close();
+	fout_->Close();
 }
 
 
@@ -195,8 +195,8 @@ fout_->Close();
 TEventList *
 EcalChannelAnalyzer::getEventListFromCut(const TCut& cut) {
 
-	t_->Draw(">>myList", cut);
-	TEventList *list = (TEventList*)gDirectory->Get("myList");
+	t_->Draw(">>List", cut);
+	TEventList *list = (TEventList*)gDirectory->Get("List");
 
 	return list;
 
@@ -209,7 +209,7 @@ void EcalChannelAnalyzer::initHistTypeMaps() {
 	h1TypeToDirectoryMap_[EcalChannelAnalyzer::H1_PED]="ecalMipHists/XtalPed";
 	h1TypeToDirectoryMap_[EcalChannelAnalyzer::H1_JITTER]="ecalMipHists/XtalJitter";
 	h1TypeToDirectoryMap_[EcalChannelAnalyzer::PROF_PULSE]="ecalMipHists/XtalPulse";
-	
+
 	h1TypeToNameMap_[EcalChannelAnalyzer::H1_AMPLI]="ampli";
 	h1TypeToNameMap_[EcalChannelAnalyzer::H1_PED]="ped";
 	h1TypeToNameMap_[EcalChannelAnalyzer::H1_JITTER]="jitter";
@@ -218,43 +218,43 @@ void EcalChannelAnalyzer::initHistTypeMaps() {
 
 void EcalChannelAnalyzer::fillEventListVector(const std::vector<std::string> & v_cuts) {
 
-for (unsigned int i=0; i<v_cuts.size(); i++) {
+	for (unsigned int i=0; i<v_cuts.size(); i++) {
 
-	TCut bufferCut = v_cuts[i].c_str();
-	TEventList eventList = *getEventListFromCut(bufferCut); 
-	v_eventList_.push_back(eventList);
+		TCut bufferCut = v_cuts[i].c_str();
+		TEventList eventList = *getEventListFromCut(bufferCut); 
+		v_eventList_.push_back(eventList);
 
-}
+	}
 
 }
 
 void EcalChannelAnalyzer::writeHistFromFile (const int hashedIndex, const int ism, const int ic, const EcalChannelAnalyzer::n_h1Type H1_TYPE) {
 
 
-//getting histogram from input file
-std::string dirbuffer = h1TypeToDirectoryMap_[H1_TYPE]+"/"+intToString(hashedIndex);
-fin_histo_->cd();
-TH1F * hist  = (TH1F*)fin_histo_->Get(dirbuffer.c_str());
+	//getting histogram from input file
+	std::string dirbuffer = h1TypeToDirectoryMap_[H1_TYPE]+"/"+intToString(hashedIndex);
+	fin_histo_->cd();
+	TH1F * hist  = (TH1F*)fin_histo_->Get(dirbuffer.c_str());
 
-//name: ism_ic_typeOfHistogram_bitmask
-//title: ism_ic_typeOfHistogram:cut1:cut2
-std::string histName = intToString(ism) + "_" + intToString(ic) + "_" + h1TypeToNameMap_[H1_TYPE] + "_" + printBitmask(xtalBitmask_[hashedIndex]); 
-std::string histTitle = intToString(ism) + "_" + intToString(ic) + "_" + h1TypeToNameMap_[H1_TYPE] + printBitmaskCuts(xtalBitmask_[hashedIndex]); 
-hist->SetName(histName.c_str());
-hist->SetTitle(histTitle.c_str());
+	//name: ism_ic_typeOfHistogram_bitmask
+	//title: ism_ic_typeOfHistogram:cut1:cut2
+	std::string histName = intToString(ism) + "_" + intToString(ic) + "_" + h1TypeToNameMap_[H1_TYPE] + "_" + printBitmask(xtalBitmask_[hashedIndex]); 
+	std::string histTitle = intToString(ism) + "_" + intToString(ic) + "_" + h1TypeToNameMap_[H1_TYPE] + printBitmaskCuts(xtalBitmask_[hashedIndex]); 
+	hist->SetName(histName.c_str());
+	hist->SetTitle(histTitle.c_str());
 
-//writing histogram on output file
-fout_->cd();
-hist->Write();
+	//writing histogram on output file
+	fout_->cd();
+	hist->Write();
 
 }
 
 std::string EcalChannelAnalyzer::intToString(int num)
 {
-    using namespace std;
-    ostringstream myStream;
-    myStream << num << flush;
-    return(myStream.str()); //returns the string form of the stringstream object
+	using namespace std;
+	ostringstream myStream;
+	myStream << num << flush;
+	return(myStream.str()); //returns the string form of the stringstream object
 }
 
 std::string EcalChannelAnalyzer::printBitmask(std::vector<bool>* bitmask) {
@@ -270,9 +270,9 @@ std::string EcalChannelAnalyzer::printBitmask(std::vector<bool>* bitmask) {
 }
 
 std::string EcalChannelAnalyzer::printBitmaskCuts(std::vector<bool>*bitmask) {
-	
+
 	std::string bitmaskCuts;
-	
+
 	for (unsigned int i=0; i< bitmask->size(); i++) {
 		if (bitmask->operator[](i)) {
 			//getting name of cut from TEntryList vector
--- plugins/EcalChannelAnalyzer.h
+++ plugins/EcalChannelAnalyzer.h
@@ -5,13 +5,13 @@
 // 
 /**\class EcalChannelAnalyzer EcalChannelAnalyzer.cc
 
- Description: <one line class summary>
+Description: <one line class summary>
 
- Implementation:
-     <Notes on implementation>
-*/
+Implementation:
+<Notes on implementation>
+ */
 // Original Author:  Caterina DOGLIONI
-//         Created:  Th Nov 22 5:46:22 CEST 2007
+//         Created:  Tu Apr 22 5:46:22 CEST 2008
 // $Id$
 //
 //
@@ -28,7 +28,6 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/Framework/interface/ESHandle.h"
 
 #include "TFile.h"
 #include "TTree.h"
@@ -37,97 +36,96 @@
 #include "TEventList.h"
 #include "TCut.h"
 
+#define MAX_XTALS 61200
+
 //
 // class declaration
 //
 
 class EcalChannelAnalyzer : public edm::EDAnalyzer {
-public:
-	explicit EcalChannelAnalyzer(const edm::ParameterSet&);
-	~EcalChannelAnalyzer();
+	public:
+		explicit EcalChannelAnalyzer(const edm::ParameterSet&);
+		~EcalChannelAnalyzer();
 
-private:
+	private:
 
-	//enum for graph type
-	enum n_h1Type { H1_AMPLI=0 , H1_PED , H1_JITTER, PROF_PULSE , NTYPES}; 
+		//enum for graph type
+		enum n_h1Type { H1_AMPLI=0 , H1_PED , H1_JITTER, PROF_PULSE , NTYPES}; 
 
 
-	virtual void beginJob(const edm::EventSetup&) ;
-	virtual void analyze(const edm::Event&, const edm::EventSetup&);
-	virtual void endJob() ;
+		virtual void beginJob(const edm::EventSetup&) ;
+		virtual void analyze(const edm::Event&, const edm::EventSetup&);
+		virtual void endJob() ;
 
-	//helpers
-	TEventList * getEventListFromCut (const TCut& cut);
-	std::string intToString(int num);
-	std::string printBitmask(std::vector<bool> * bitmask);
-	std::string printBitmaskCuts(std::vector<bool> * bitmask);
-	void initHistTypeMaps();
-	void fillEventListVector(const std::vector<std::string>& v_cuts);   	
-	void writeHistFromFile(int hashedIndex, int ism, int ic, n_h1Type H1TYPE); 
+		//helpers
+		TEventList * getEventListFromCut (const TCut& cut);
+		std::string intToString(int num);
+		std::string printBitmask(std::vector<bool> * bitmask);
+		std::string printBitmaskCuts(std::vector<bool> * bitmask);
+		void initHistTypeMaps();
+		void fillEventListVector(const std::vector<std::string>& v_cuts);   	
+		void writeHistFromFile(int hashedIndex, int ism, int ic, n_h1Type H1TYPE); 
 
-// ----------member data --------------------------
+		// ----------member data --------------------------
 
-//tree
-TTree * t_;
-//filename for input (where the TTree will be retrieved)
-std::string inputTreeFileName_;
-std::string inputHistoFileName_;
-std::string outputFileName_;
+		//tree
+		TTree * t_;
 
-std::vector<std::string> v_cuts_;
+		//filenames
+		std::string inputTreeFileName_;
+		std::string inputHistoFileName_;
+		std::string outputFileName_;
 
-TFile * fin_tree_;
-TFile * fin_histo_;
-TFile * fout_;
+		//vectors for cuts/event lists
+		std::vector<std::string> v_cuts_;
+		std::vector <TEventList> v_eventList_;
+		//total TEventList (no duplicate crystals)
+		TEventList totalEventList_;
 
-//tree variables
+		//vector for bitmasks (vector<bool> bitmask[i] = 1 ->  crystal selected for v_cuts_[i])
+		std::vector<bool> * xtalBitmask_[MAX_XTALS];
+		//number of cuts
+		int nCuts_;
 
-int	ic;
-int     ism;
-int     ieta;
-int     iphi;
-int     hashedIndex;
-float   ped_avg;
-float   ped_rms;
-float   ampli_avg;
-float   ampli_rms;
-float   jitter_avg;
-float   jitter_rms;
-float   ampli_fracBelowThreshold;
-int     entries;
+		//maps
+		//map for directories:hist type
+		std::map < EcalChannelAnalyzer::n_h1Type , std::string > h1TypeToDirectoryMap_;
+		//map for hist name:hist type
+		std::map < EcalChannelAnalyzer::n_h1Type , std::string > h1TypeToNameMap_;
 
-//branches
+		//files
+		TFile * fin_tree_;
+		TFile * fin_histo_;
+		TFile * fout_;
 
-TBranch        *b_ic;   //!
-TBranch        *b_ism;   //!
-TBranch        *b_ieta;   //!
-TBranch        *b_iphi;   //!
-TBranch        *b_hashedIndex;   //!
-TBranch        *b_ped_avg;   //!
-TBranch        *b_ped_rms;   //!
-TBranch        *b_ampli_avg;   //!
-TBranch        *b_ampli_rms;   //!
-TBranch        *b_jitter_avg;   //!
-TBranch        *b_jitter_rms;   //!
-TBranch        *b_ampli_fracBelowThreshold;   //!
-TBranch        *b_entries;   //!
+		//tree variables
+		int	ic;
+		int     ism;
+		int     ieta;
+		int     iphi;
+		int     hashedIndex;
+		float   ped_avg;
+		float   ped_rms;
+		float   ampli_avg;
+		float   ampli_rms;
+		float   jitter_avg;
+		float   jitter_rms;
+		float   ampli_fracBelowThreshold;
+		int     entries;
 
-//map for directories:hist type
-std::map < EcalChannelAnalyzer::n_h1Type , std::string > h1TypeToDirectoryMap_;
-
-//map for hist name:hist type
-std::map < EcalChannelAnalyzer::n_h1Type , std::string > h1TypeToNameMap_;
-
-//vector for TEventLists (from TCuts)
-std::vector <TEventList> v_eventList_;
-
-//total TEventList (no duplicate crystals)
-TEventList totalEventList_;
-
-//vector for bitmasks (which cuts?)
-std::vector<bool> * xtalBitmask_[61200];
-
-//number of cuts
-int nCuts_;
+		//branches
+		TBranch        *b_ic;   //!
+		TBranch        *b_ism;   //!
+		TBranch        *b_ieta;   //!
+		TBranch        *b_iphi;   //!
+		TBranch        *b_hashedIndex;   //!
+		TBranch        *b_ped_avg;   //!
+		TBranch        *b_ped_rms;   //!
+		TBranch        *b_ampli_avg;   //!
+		TBranch        *b_ampli_rms;   //!
+		TBranch        *b_jitter_avg;   //!
+		TBranch        *b_jitter_rms;   //!
+		TBranch        *b_ampli_fracBelowThreshold;   //!
+		TBranch        *b_entries;   //!
 
 };
